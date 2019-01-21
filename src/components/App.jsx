364cbf13540f520326a2de1dccf27be8f8816776
Brian Lagman <bjlagman@gmail.com>
2019-01-21 13:48:44 -0800

    functional form input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,45 +2,44 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      value: ''
-    };  
-    // this.handleChange = this.handleChange.bind(this)
- }
- handleChange(e) {
-   this.setState({value:e.target.value})
- };
+      name: '',
+      message: ''
+    };
+
+    this.handleChangeName = this.handleChangeName.bind(this)
+    this.handleChangeMessage = this.handleChangeMessage.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeName(event) {
+    this.setState({name: event.target.value});
+  }
+  handleChangeMessage(event) {
+    this.setState({message:event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.name + this.state.message);
+    // event.preventDefault;
+  }
 
   render() {
     return (
-    <div>
-      <h4>
-        Server Response:
-      </h4>
-      <h4>
-        Response goes here!
-      </h4>
-    <form>
-      <label>
-        Name:
-        <input 
-          className='name-form' 
-          type='text' 
-          value={this.state.value} 
-          onChange={this.handleChange}/>
-      </label>
-      <label>
-        Message:
-        <input 
-          className='name-form' 
-          type='text' 
-          value={this.state.value} 
-          onChange={this.handleChange}/>
-      </label>
-        <input type='submit' value='Submit'/>
-    </form>
-    </div> 
+       <div>
+      <h2>Server Response:</h2>
+      <h2>Response goes here!</h2>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.name} onChange={this.handleChangeName} />
+          Message:
+          <input type="text" value={this.state.message} onChange={this.handleChangeMessage} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      </div>
     );
   }
 }
